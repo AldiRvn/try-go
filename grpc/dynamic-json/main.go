c141ed18b6bbd4f1b396c351763cadf62b19140c
Arawan <45452761+AldiRvn@users.noreply.github.com>
2021-11-01 07:40:08 +0700

    Create main.go

---
+++ grpc/dynamic-json/main.go
@@ -0,0 +1,42 @@
+// Using arbitary JSON / map[string]interface in grpc
+package main
+
+import (
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+func main() {
+	data, err := structpb.NewValue(map[string]interface{}{
+		"name":    "Ricardo Hogan",
+		"country": "Guam",
+		"date":    "29/05/2021",
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dataAsModel := stub.Response{
+		Message: "OK",
+		Data:    data.GetStructValue(),
+	}
+	asBytes, _ := json.MarshalIndent(
+		&dataAsModel, "", "  ")
+	fmt.Println(string(asBytes))
+
+	for key, field := range dataAsModel.Data.Fields {
+		fmt.Println(key, field)
+	}
+
+  //* output
+  // {
+  //   "message": "OK",
+  //   "data": {
+  //     "country": "Guam",
+  //     "date": "29/05/2021",
+  //     "name": "Ricardo Hogan"
+  //   }
+  // }
+  // name string_value:"Ricardo Hogan"
+  // country string_value:"Guam"
+  // date string_value:"29/05/2021"
+}
